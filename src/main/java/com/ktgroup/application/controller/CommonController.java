53dfd79da3d041fe42217806c393e26a8d8fe12f
doan2021 <doan81391@gmail.com>
2021-04-25 19:58:35 +0700

    create login and register

--- src/main/java/com/ktgroup/application/controller/CommonController.java
+++ src/main/java/com/ktgroup/application/controller/CommonController.java
@@ -2,6 +2,7 @@ package com.ktgroup.application.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ktgroup.application.dto.AppUserForm;
+import com.ktgroup.application.services.AppUserServices;
+import com.ktgroup.application.services.RoleServices;
 import com.ktgroup.application.utils.WebUtils;
+import com.ktgroup.application.validator.AppUserValidator;
 
 @Controller
 public class CommonController {
+    
+    @Autowired
+    AppUserServices appUserServices;
+
+    @Autowired
+    RoleServices roleServices;
+    
     @GetMapping(value = { "/", "/login"})
     public String welcomePage(Model model) {
         return "login";
@@ -30,5 +42,11 @@ public class CommonController {
             model.addAttribute("message", message);
         }
         return "403Page";
+    }
+    
+    @GetMapping(value = {"/register"})
+    public String registerPage(Model model) {
+        model.addAttribute("appUserForm", new AppUserForm());
+        return "Register";
     }
 }
