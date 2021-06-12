a557eb6f525e3677a9d8af934b4d6c1910361e56
doan2021 <doan81391@gmail.com>
2021-06-12 21:49:58 +0700

    coding user

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -8,11 +8,11 @@ window.onscroll = function() {
 
 function scrollFunction() {
 	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-		$("#navbar").removeClass("py-4 bd-transparent navbar-dark");
+		$("#navbar").removeClass("py-3 bd-transparent navbar-dark");
 		$("#navbar").addClass("py-2 bg-light navbar-light");
 		
 	} else {
 		$("#navbar").removeClass("py-2 bg-light navbar-light");
-		$("#navbar").addClass("py-4 bd-transparent navbar-dark");
+		$("#navbar").addClass("py-3 bd-transparent navbar-dark");
 	}
 }
